5ec5ea3f9917279e958510ee0bf07b1f6f68ea9e
victoriavianx <victoriagarcia963@gmail.com>
2022-09-19 12:24:28 -0300

    feat: create user migration

--- src/migrations/1663597604946-createUser.ts
+++ src/migrations/1663597604946-createUser.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class createUser1656976841608 implements MigrationInterface {
-    name = 'createUser1656976841608'
+export class createUser1663597604946 implements MigrationInterface {
+    name = 'createUser1663597604946'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, "age" integer NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
